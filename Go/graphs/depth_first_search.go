29485130145df61267728b03255758a3d31c233c
Shubham Agarwal <to.shubham.ag@gmail.com>
2021-03-15 18:50:33 +0530

    dfs in golang added

--- Go/graphs/depth_first_search.go
+++ Go/graphs/depth_first_search.go
@@ -13,122 +13,112 @@ type node struct {
 	next *node
 }
 
-func DFS(g *Graph, startVertex *Vertex, visitCb func(int)) {
-	// we maintain a map of visited nodes to prevent visiting the same
-	// node more than once
+func DFS(g *Graph, root *Vertex, visit func(int)) {
+	
+	//maintaining the map of all visited node
 	visited := map[int]bool{}
 
-	if startVertex == nil {
+	if root == nil {
 		return
 	}
-	visited[startVertex.Key] = true
-	visitCb(startVertex.Key)
 
-	// for each of the adjacent vertices, call the function recursively
-	// if it hasn't yet been visited
-	for _, v := range startVertex.Vertices {
-		if visited[v.Key] {
+	visited[root.info] = true
+	visit(root.info)
+
+	//calling the same DFS for all its adjacent
+	for _, v := range root.Vertices {
+		if visited[v.info] {
 			continue
 		}
-		DFS(g, v, visitCb)
+		DFS(g, v, visit)
 	}
 }
 
 type Vertex struct {
-	// Key is the unique identifier of the vertex
-	Key int
+	// the data
+	info int
 	//vertices connecting to other vertex
 	Vertices map[int]*Vertex
 }
 
-// We then create a constructor function for the Vertex
-func NewVertex(key int) *Vertex {
+//creating a new vertex
+func NewVertex(info int) *Vertex {
 	return &Vertex{
-		Key:      key,
+		info:      info,
 		Vertices: map[int]*Vertex{},
 	}
 }
 
 func (v *Vertex) String() string {
-	s := strconv.Itoa(v.Key) + ":"
+	s := strconv.Itoa(v.info) + ":"
 
-	for _, neighbor := range v.Vertices {
-		s += " " + strconv.Itoa(neighbor.Key)
+	for _, next := range v.Vertices {
+		s += " " + strconv.Itoa(next.info)
 	}
 
 	return s
 }
 
 type Graph struct {
-	// Vertices describes all vertices contained in the graph
-	// The key will be the Key value of the connected vertice
-	// with the value being the pointer to it
+	//map for all vertices connected to each other
 	Vertices map[int]*Vertex
 	// This will decide if it's a directed or undirected graph
 	directed bool
 }
 
-// We defined constructor functions that create
-// new directed or undirected graphs respectively
-
-func NewDirectedGraph() *Graph {
+//creating a new graph
+func NewGraph() *Graph {
 	return &Graph{
 		Vertices: map[int]*Vertex{},
 		directed: true,
 	}
 }
 
-// AddVertex creates a new vertex with the given
-// key and adds it to the graph
-func (g *Graph) AddVertex(key int) {
-	v := NewVertex(key)
-	g.Vertices[key] = v
+//function for adding vertex
+func (g *Graph) AddVertex(info int) {
+	v := NewVertex(info)
+	g.Vertices[info] = v
 }
 
-// The AddEdge method adds an edge between two vertices in the graph
+// function for adding edge
 func (g *Graph) AddEdge(k1, k2 int) {
 	v1 := g.Vertices[k1]
 	v2 := g.Vertices[k2]
 
-	// return an error if one of the vertices doesn't exist
 	if v1 == nil || v2 == nil {
-		panic("not all vertices exist")
-	}
-
-	// do nothing if the vertices are already connected
-	if _, ok := v1.Vertices[v2.Key]; ok {
 		return
 	}
 
-	// Add a directed edge between v1 and v2
-	// If the graph is undirected, add a corresponding
-	// edge back from v2 to v1, effectively making the
-	// edge between v1 and v2 bidirectional
-	v1.Vertices[v2.Key] = v2
-	if !g.directed && v1.Key != v2.Key {
-		v2.Vertices[v1.Key] = v1
+	if _, ok := v1.Vertices[v2.info]; ok {
+		return
 	}
 
-	// Add the vertices to the graph's vertex map
-	g.Vertices[v1.Key] = v1
-	g.Vertices[v2.Key] = v2
+	//adding a directed edge
+	v1.Vertices[v2.info] = v2
+	if !g.directed && v1.info != v2.info {
+		v2.Vertices[v1.info] = v1
+	}
+
+	// adding the vertices
+	g.Vertices[v1.info] = v1
+	g.Vertices[v2.info] = v2
 }
 
 func (g *Graph) String() string {
-	s := ""
+	st := ""
 	i := 0
-	for _, v := range g.Vertices {
+	for _, vertex := range g.Vertices {
 		if i != 0 {
-			s += "\n"
+			st += "\n"
 		}
-		s += v.String()
+		st += vertex.String()
 		i++
 	}
-	return s
+	return st
 }
 
 func main() {
-	g := NewDirectedGraph()
+	g := NewGraph()
 
 	g.AddVertex(1)
 	g.AddVertex(2)
@@ -157,7 +147,6 @@ func main() {
 	}
 	DFS(g, g.Vertices[1], cb)
 
-	// add assertions here
 	fmt.Print("Depth First Search: ")
 	fmt.Println(visitedOrder)
 }
